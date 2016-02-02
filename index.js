f434ba7069161738dac6e6759f02cdda590b8bdf
Justin Leung <justin.leung881@gmail.com>
2016-02-01 18:04:45 -0600

    Added functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var qs = require('querystring');
+var func = require('./module.js')
 
 // Twilio Credentials (preset in Heroku configuration)
 var accountSid = process.env.accountSid;
@@ -53,13 +54,61 @@ var server = http.createServer(function (req,res) {
       if (masterArray.indexOf(phoneNumber) === -1) {
         //Add phoneNumber to the master list of numbers
       };
-      
-      if (textArray[0] === '@help') {
+      switch(textArray[0]){
+        case '@help':
+          break;
+        case '@newlist':
+          outputText = func.newList(textArray[1],phoneNumber);
+          break;
+        case '@closelist':
+          outputText = func.clearNumber(textArray[1],phoneNumber);
+          break;
+        case '@checklist':
+          outputText = func.checkList(textArray[1],phoneNumber);
+          break;
+        case '@additem':
+          var tempAddItems = '';
+          for(i = 2; i < textArray.length - 1; i++){
+            func.addItem(textArray[1],phoneNumber,textArray[i]);
+            tempAddItems = tempAddItems + textArray[i] + ', ';
+          };
+          outputText = 'Added ' + tempAddItems; 
+          break;
+        case '@removeitem':
+          var tempDelItems = '';
+          for(i = 2; i < textArray.length - 1; i++){
+            func.delItem(textArray[1],phoneNumber,textArray[i]);
+            tempDelItems = tempDelItems + textArray[i] + ', ';
+          };
+          outputText = 'Deleted ' + tempDelItems;
+          break;
+        case '@addnumber':
+          var tempAddNumb = '';
+          for(i = 2; i < textArray.length - 1; i++){
+            func.addNumber(textArray[1],phoneNumber,textArray[i]);
+            tempAddNumb = tempAddNumb + textArray[i] + ', ';
+          };
+          outputText = 'Added ' + tempAddNumb;
+          break;
+        case '@removenumber'
+          var tempDelNumb = '';
+          for(i = 2; i < textArray.length - 1; i++){
+            func.delNumber(textArray[1],phoneNumber,textArray[i]);
+            tempDelNumb = tempDelNumb + textArray[i] + ', ';
+          };
+          outputText = 'Added ' + tempDelNumb;
+          break;
+        default:
+          outputText = 'GrocerySMS does not recognize that command. Use @help if you need a list of commands.';
+          break;
+      };
+      /*if (textArray[0] === '@help') {
         //Show a list of commands and brief explanations
       }
 
       else if (textArray[0] === '@newlist') {
         //Create a new grocery list
+        outputText = module.addList(code,number);
       }
 
       else if (textArray[0] === '@closelist') {
@@ -89,7 +138,7 @@ var server = http.createServer(function (req,res) {
       else {
         //Return an error
         outputText = 'GrocerySMS does not recognize that command. Use @help if you need a list of commands.';
-      };
+      };*/
 
       res.writeHead(200, {'Content-Type': 'text/xml'});
       res.write('<?xml version="1.0" encoding="UTF-8" ?>');
