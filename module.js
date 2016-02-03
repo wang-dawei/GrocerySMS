e2e147ac109f91e5e0a6b480cd33e308e14e467c
Justin Leung <justin.leung881@gmail.com>
2016-02-03 14:17:34 -0600

    revamped datastructures

--- module.js
+++ module.js
@@ -1,79 +1,48 @@
-//module.js
-var exports = module.exports = {};
+var exports = module.exports{};
 
-//Checking a list using a code key
-exports.checkList = function(code,number){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			return key[code][1];
+//New list
+exports.newList = function(masterDict,number,key,listName,lists,code){
+	code++;
+
+	lists[code] = [];
+	key[listName] = code;
+	masterDict[number] = key;
+};
+
+//Delete list
+exports.clearList = function(masterDict,number,key,listName,lists){
+	var code = masterDict[number][key][listName]; //Retrieves code
+
+	delete lists[code];
+};
+
+//Checks a list
+exports.checkList = function(masterDict,number,key,listName,lists,item){
+	var code = masterDict[number][key][listName]; //Retrieves code
+
+	return lists[code];
+};
+
+//Add Item to list
+exports.addItem = function(masterDict,number,key,listName,lists,item){
+	var code = masterDict[number][key][listName]; //Retrieves code
+
+	lists[code].push(item);
+};
+
+//Delete Item from list
+exports.delItem = function(masterDict,number,key,listName,lists,item){
+	var code = masterDict[number][key][listName]; //Retrieves code
+
+	for(i = 0; i < lists[code].length; i++){
+		if(lists[code][i] === item){
+			lists[code].splice(i,1);
 		};
 	};
 };
 
-//Add an item
-exports.addItem = function(code,number,item){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			//Check if item is repeated and lower case them
-			key[code][1].push(item);
-		};
-	};
-};
+exports.addNumber = function(masterDict,origNumber,key,listName,newNumber,newListName){
+	var code = masterDict[origNumber][key][listName]; //Retrieves code for a specific list to share
 
-//Delete an item
-exports.deleteItem = function(code,number,item){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			for(j = 0; j < key[code][1].length; j++){
-				if(key[code][1][j] == item){
-					key[code][1].splice(j,1);
-				};
-			};
-		};
-	};
-};
-
-//Clear list
-exports.clearList = function(code,number){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			delete key[code];
-			return 'Deleted';
-		};
-	};
-};
-
-//Add list
-exports.addList = function(code,number){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0].length === 0){
-			key[code][0] = number;
-			return 'Success';
-		}
-		else{
-			return 'Code taken, try again';
-		};
-	};
-};
-
-//Add Number
-exports.addNumber = function(code,number,newNum){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			key[code][0].push(newNum);
-		};
-	};
-};
-
-//Remove Number
-exports.delNumber = function(code,number,delNum){
-	for(i = 0; i < key[code][0].length; i++){
-		if(key[code][0][i] === number){
-			for(j = 0; j < key[code][0]; j++){
-				if(key[code][0][j] == delNum){
-					key[code][0].splice(j,1);
-				}
-			};
-		};
-	};
+	masterDict[newNumber][key][newListName] = code;
 };
