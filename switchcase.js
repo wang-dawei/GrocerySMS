025e9580264ed23e0da12e2c927fd58729bbd08d
Justin Leung <justin.leung881@gmail.com>
2016-02-23 19:28:58 -0600

    test 3

---
+++ switchcase.js
@@ -0,0 +1,17 @@
+switch ('hello') {
+	case 'hello':
+		console.log(0);
+		break;
+	case 1:
+		console.log(1);
+		break;
+	case 2:
+		console.log(2);
+		break;
+	case 3:
+		console.log(3);
+		break;
+	case 4:
+		console.log(4);
+		break;
+};
