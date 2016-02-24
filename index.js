c26222e750c5e1ec7f7a07104507fa8e637eabce
Justin Leung <justin.leung881@gmail.com>
2016-02-23 19:05:59 -0600

    more changes to index.js and module.js

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ var accountSid = process.env.accountSid;
 var authToken = process.env.authToken;
  
 //require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
+var client = require('twilio')(accountSid, authToken);
+
+//Data Arrays
+var masterDict = [];
+var lists = [];
+var code = 0;
 
 var server = http.createServer(function (req,res) {
   parsedURL = url.parse(req.url,true);
@@ -21,6 +26,7 @@ var server = http.createServer(function (req,res) {
     res.write('<title>GrocerySMS</title>');
     res.write('</head>');
     res.write('<body>');
+    res.write('<div align = "center">');
     res.write('<h1>');
     res.write('GrocerySMS');
     res.write('</h1>');
@@ -30,6 +36,7 @@ var server = http.createServer(function (req,res) {
     res.write('<p>');
     res.write('Commands:')
     res.write('</p>');
+    res.write('</div>');
     res.write('</body>');
     res.write('</html>');
     res.end();
@@ -58,18 +65,18 @@ var server = http.createServer(function (req,res) {
         case '@help':
           break;
         case '@newlist':
-          outputText = func.newList(textArray[1],phoneNumber);
+          outputText = func.newList(masterDict,phoneNumber,textArray[1],lists,code);
           break;
         case '@closelist':
-          outputText = func.clearNumber(textArray[1],phoneNumber);
+          outputText = func.clearList(masterDict,phoneNumber,textArray[1],lists);
           break;
         case '@checklist':
-          outputText = func.checkList(textArray[1],phoneNumber);
+          outputText = func.checkList(masterDict,phoneNumber,textArray[1],lists);
           break;
         case '@additem':
           var tempAddItems = '';
           for(i = 2; i < textArray.length - 1; i++){
-            func.addItem(textArray[1],phoneNumber,textArray[i]);
+            func.addItem(masterDict,phoneNumber,textArray[1],lists,textArray[i]);
             tempAddItems = tempAddItems + textArray[i] + ', ';
           };
           outputText = 'Added ' + tempAddItems; 
@@ -77,27 +84,20 @@ var server = http.createServer(function (req,res) {
         case '@removeitem':
           var tempDelItems = '';
           for(i = 2; i < textArray.length - 1; i++){
-            func.delItem(textArray[1],phoneNumber,textArray[i]);
+            func.delItem(masterDict,phoneNumber,textArray[1],lists,textArray[i]);
             tempDelItems = tempDelItems + textArray[i] + ', ';
           };
           outputText = 'Deleted ' + tempDelItems;
           break;
         case '@addnumber':
           var tempAddNumb = '';
-          for(i = 2; i < textArray.length - 1; i++){
-            func.addNumber(textArray[1],phoneNumber,textArray[i]);
+          for(i = 3; i < textArray.length - 1; i++){
+            func.addNumber(masterDict,phoneNumber,textArray[1],textArray[i],textArray[2]);
             tempAddNumb = tempAddNumb + textArray[i] + ', ';
           };
           outputText = 'Added ' + tempAddNumb;
           break;
-        case '@removenumber'
-          var tempDelNumb = '';
-          for(i = 2; i < textArray.length - 1; i++){
-            func.delNumber(textArray[1],phoneNumber,textArray[i]);
-            tempDelNumb = tempDelNumb + textArray[i] + ', ';
-          };
-          outputText = 'Added ' + tempDelNumb;
-          break;
+
         default:
           outputText = 'GrocerySMS does not recognize that command. Use @help if you need a list of commands.';
           break;
--- module.js
+++ module.js
@@ -1,38 +1,39 @@
 var exports = module.exports{};
 
 //New list
-exports.newList = function(masterDict,number,key,listName,lists,code){
+exports.newList = function(masterDict,number,listName,lists,code){
 	code++;
 
 	lists[code] = [];
-	key[listName] = code;
-	masterDict[number] = key;
+	masterDict[number] = [];
+	masterDict[number][listName] = code;
 };
 
 //Delete list
-exports.clearList = function(masterDict,number,key,listName,lists){
-	var code = masterDict[number][key][listName]; //Retrieves code
+exports.clearList = function(masterDict,number,listName,lists){
+	var code = masterDict[number][listName]; //Retrieves code
+
 
 	delete lists[code];
 };
 
 //Checks a list
-exports.checkList = function(masterDict,number,key,listName,lists,item){
-	var code = masterDict[number][key][listName]; //Retrieves code
+exports.checkList = function(masterDict,number,listName,lists){
+	var code = masterDict[number][listName]; //Retrieves code
 
 	return lists[code];
 };
 
 //Add Item to list
-exports.addItem = function(masterDict,number,key,listName,lists,item){
-	var code = masterDict[number][key][listName]; //Retrieves code
+exports.addItem = function(masterDict,number,listName,lists,item){
+	var code = masterDict[number][listName]; //Retrieves code
 
 	lists[code].push(item);
 };
 
 //Delete Item from list
-exports.delItem = function(masterDict,number,key,listName,lists,item){
-	var code = masterDict[number][key][listName]; //Retrieves code
+exports.delItem = function(masterDict,number,listName,lists,item){
+	var code = masterDict[number][listName]; //Retrieves code
 
 	for(i = 0; i < lists[code].length; i++){
 		if(lists[code][i] === item){
@@ -41,8 +42,8 @@ exports.delItem = function(masterDict,number,key,listName,lists,item){
 	};
 };
 
-exports.addNumber = function(masterDict,origNumber,key,listName,newNumber,newListName){
-	var code = masterDict[origNumber][key][listName]; //Retrieves code for a specific list to share
+exports.addNumber = function(masterDict,origNumber,listName,newNumber,newListName){
+	var code = masterDict[origNumber][listName]; //Retrieves code for a specific list to share
 
-	masterDict[newNumber][key][newListName] = code;
+	masterDict[newNumber][newListName] = code;
 };
