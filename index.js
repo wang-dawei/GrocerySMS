7830e1d90495508bc9bf2257b728565872806a40
David <davidwang@utexas.edu>
2016-02-29 23:29:24 -0600

    Adding commands functionality v1

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var func = require('./module.js');
 var accountSid = process.env.accountSid;
 var authToken = process.env.authToken;
  
+//Data Structures
+var masterDict = [];
+var lists = [];
+var code = 0;
+
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
 
@@ -50,24 +55,24 @@ var server = http.createServer(function (req,res) {
       var phoneNumber = requestObject.From;
       var textArray = requestObject.Body.split(' ');
       console.log(textArray);
-      /*var outputText = requestObject.Body;
+      var outputText = requestObject.Body;
 
       switch(textArray[0]){
         case '@help':
           break;
         case '@newlist':
-          outputText = func.newList(textArray[1],phoneNumber);
+          outputText = func.newList(masterDict,phoneNumber,textArray[1],lists,code);
           break;
         case '@closelist':
-          outputText = func.clearNumber(textArray[1],phoneNumber);
+          outputText = func.clearNumber(masterDict,phoneNumber,textArray[1],lists);
           break;
         case '@checklist':
-          outputText = func.checkList(textArray[1],phoneNumber);
+          outputText = func.checkList(masterDict,phoneNumber,textArray[1],lists);
           break;
         case '@additem':
           var tempAddItems = '';
           for(i = 2; i < textArray.length - 1; i++){
-            func.addItem(textArray[1],phoneNumber,textArray[i]);
+            func.addItem(masterDict,phoneNumber,textArray[1],lists,textArray[i]);
             tempAddItems = tempAddItems + textArray[i] + ', ';
           };
           outputText = 'Added ' + tempAddItems; 
@@ -75,36 +80,28 @@ var server = http.createServer(function (req,res) {
         case '@removeitem':
           var tempDelItems = '';
           for(i = 2; i < textArray.length - 1; i++){
-            func.delItem(textArray[1],phoneNumber,textArray[i]);
+            func.delItem(masterDict,phoneNumber,textArray[1],lists,textArray[i]);
             tempDelItems = tempDelItems + textArray[i] + ', ';
           };
           outputText = 'Deleted ' + tempDelItems;
           break;
         case '@addnumber':
           var tempAddNumb = '';
-          for(i = 2; i < textArray.length - 1; i++){
-            func.addNumber(textArray[1],phoneNumber,textArray[i]);
+          for(i = 3; i < textArray.length - 1; i++){
+            func.addNumber(masterDict,phoneNumber,textArray[1],textArray[i],textArray[2]);
             tempAddNumb = tempAddNumb + textArray[i] + ', ';
           };
           outputText = 'Added ' + tempAddNumb;
           break;
-        case '@removenumber'
-          var tempDelNumb = '';
-          for(i = 2; i < textArray.length - 1; i++){
-            func.delNumber(textArray[1],phoneNumber,textArray[i]);
-            tempDelNumb = tempDelNumb + textArray[i] + ', ';
-          };
-          outputText = 'Added ' + tempDelNumb;
-          break;
         default:
           outputText = 'GrocerySMS does not recognize that command. Use @help if you need a list of commands.';
           break;
-      };*/
+      };
 
       res.writeHead(200, {'Content-Type': 'text/xml'});
       res.write('<?xml version="1.0" encoding="UTF-8" ?>');
       res.write('<Response>');
-      res.write('<Message>GrocerySMS: Hello!</Message>');
+      res.write('<Message>GrocerySMS: ' + outputText + '</Message>');
       res.write('</Response>');
       res.end();
     });
